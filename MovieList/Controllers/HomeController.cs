c2cee4a6fd53069e7d705de6ef4f97134898380e
Guilherme Malosti Fonseca <guilherme.fonseca@gsw.com.br>
2023-01-16 15:01:46 -0300

    Adding bootstrap, about page

--- MovieList/Controllers/HomeController.cs
+++ MovieList/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace MovieList.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
