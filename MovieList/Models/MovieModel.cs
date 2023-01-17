f054909376ea5612f913450c154a44bdadb0ba64
Guilherme Malosti Fonseca <guilherme.fonseca@gsw.com.br>
2023-01-17 12:14:23 -0300

    adding more style

--- MovieList/Models/MovieModel.cs
+++ MovieList/Models/MovieModel.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieList.Models
 {
     public class MovieModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is mandatory")]
+        [MaxLength(80)]
         public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Year")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy}")]
         public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Rate the movie is mandatory, please choose a grade between 0 and 10")]
+        [DisplayFormat(DataFormatString = "{0} / 10")]
         public float Rating { get; set; }
 
         public MovieModel()
--- MovieList/Startup.cs
+++ MovieList/Startup.cs
@@ -1,15 +1,13 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MovieList.Data;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using System.Collections.Generic;
 
 namespace MovieList
 {
@@ -35,6 +33,15 @@ namespace MovieList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo>{enUS},
+                SupportedUICultures = new List<CultureInfo> {enUS}
+            };
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
